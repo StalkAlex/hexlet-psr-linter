8e703f01dd3a3b04df1cedd5b1d7e8fb4baade54
Александр Абоимов <a.aboimov@zko.nadir.kz>
2016-08-03 16:58:56 +0500

    Enable Cilex application, add LintCommand class

---
+++ src/LintCommand.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: AlexandrAboimov
+ * Date: 03.08.2016
+ * Time: 16:43
+ */
+
+namespace HexletPstLinter;
+
+
+use Cilex\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Runs linter process
+ */
+class LintCommand extends Command
+{
+    protected function configure()
+    {
+        $this
+            ->setName('demo:info')
+            ->setDescription('Analyze your php code');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+    }
+
+}
---
+++ tests/LintCommandTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: AlexandrAboimov
+ * Date: 03.08.2016
+ * Time: 16:57
+ */
+
+namespace HexletPsrLinterTest;
+
+
+class LintCommandTest
+{
+
+}
